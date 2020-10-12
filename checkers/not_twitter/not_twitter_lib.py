59594e1de904836aec90cce848ea3864f92f42f8
amoniaka-knabino <sv@dynfor.ru>
2020-10-12 12:05:55 +0300

    add status as last arg to assert functions

--- checkers/not_twitter/not_twitter_lib.py
+++ checkers/not_twitter/not_twitter_lib.py
@@ -23,54 +23,54 @@ class CheckMachine:
     
     def register(self, session, username, password, status=Status.MUMBLE):
         r = session.post(self.url + '/register', data = {'login':username, 'password': password})
-        self.c.assert_eq(200, r.status_code, "Can't register")
+        self.c.assert_eq(200, r.status_code, "Can't register", status=status)
         #print(r.status_code)
 
     def login(self, session, username, password, status=Status.MUMBLE):
         r = session.post(self.url + '/login', data = {'login':username, 'password': password})
-        self.c.assert_eq(200, r.status_code, "Can't login")
+        self.c.assert_eq(200, r.status_code, "Can't login", status=status)
         #print(r.status_code)
     
-    def _upload_file_by_link(self, session, link):
+    def _upload_file_by_link(self, session, link, status):
         """
         return filename of uploaded file
         """
         #example_link = f"file:///services/not_twitter/uploads/{filename}"
         r = session.post(self.url + '/upload_by_link', data = {'link':link})
-        self.c.assert_eq(200, r.status_code, "Can't upload file by link")
+        self.c.assert_eq(200, r.status_code, "Can't upload file by link", status=status)
         new_link = new_filename_link.findall(r.text)[0]
         return new_link
     
-    def upload_text(self, session, text):
+    def upload_text(self, session, text, status):
         """
         return filename of uploaded file
         """
         r = session.post(self.url + '/upload', data = {'text':text})
-        self.c.assert_eq(200, r.status_code, "Can't upload text")
+        self.c.assert_eq(200, r.status_code, "Can't upload text", status=status)
         new_link = new_filename_link.findall(r.text)[0]
         return new_link
 
     
-    def _download_file_by_link(self, session, link):
+    def _download_file_by_link(self, session, link, status):
         """
         return file content
         """
         r = session.get(self.url + '/' + link)
-        self.c.assert_eq(200, r.status_code, "Can't download file")
+        self.c.assert_eq(200, r.status_code, "Can't download file", status=status)
         return r.text
     
     def check_file_content_by_link(self, session, link, expected_content, status = Status.MUMBLE):
-        file_content = self._download_file_by_link(session, link)
+        file_content = self._download_file_by_link(session, link, status)
         self.c.assert_eq(file_content, expected_content, "Can't upload text", status=status)
         #print(file_content == expected_content, file_content, expected_content)
 
     def check_upload_by_link(self, session, link, status=Status.MUMBLE):
         link_content = urllib.request.urlopen(link).read(MAX_FILESIZE).decode('utf-8')
-        server_link = self._upload_file_by_link(session, link)
-        link_content_on_server = self._download_file_by_link(session, server_link)
-        self.c.assert_eq(link_content, link_content_on_server, "Can't upload file by link")
+        server_link = self._upload_file_by_link(session, link, status)
+        link_content_on_server = self._download_file_by_link(session, server_link, status)
+        self.c.assert_eq(link_content, link_content_on_server, "Can't upload file by link", status=status)
         #print(link_content_on_server == link_content)
-        self._check_filename_in_uploads_page(session, server_link, status)
+        self._check_filename_in_uploads_page(session, server_link, status=status)
     
     def _get_uploads_list(self, s):
         limit = 128
