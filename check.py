6525b97a8596d62c818dd5ff1b996de4523d1810
Roman Nikitin <nikrom2012@me.com>
2020-10-13 09:55:12 +0300

    Updated .gitignore, added new check.py tests runner

---
+++ check.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import random
+import secrets
+import string
+import subprocess
+import traceback
+from pathlib import Path
+from threading import Thread, Lock, current_thread
+from typing import List, Tuple
+
+BASE_DIR = Path(__file__).resolve().absolute().parent
+SERVICES_PATH = BASE_DIR / 'services'
+CHECKERS_PATH = BASE_DIR / 'checkers'
+TRAVIS_PATH = BASE_DIR / '.travis.yml'
+RUNS = int(os.getenv('RUNS', default=10))
+HOST = os.getenv('HOST', default='127.0.0.1')
+OUT_LOCK = Lock()
+
+
+def generate_flag(name):
+    alph = string.ascii_uppercase + string.digits
+    return name[0].upper() + ''.join(random.choices(alph, k=30)) + '='
+
+
+class Checker:
+    def __init__(self, name: str):
+        self._name = name
+        self._exe_path = CHECKERS_PATH / self._name / 'checker.py'
+        assert os.access(self._exe_path,
+                         os.X_OK), f'checker {self._name}: {self._exe_path.relative_to(BASE_DIR)} must be executable'
+        self._timeout = 3
+        self._get_info()
+
+    def _get_info(self):
+        self._log('running info action')
+        cmd = [str(self._exe_path), 'info', HOST]
+        out, _ = self._run_command(cmd)
+        info = json.loads(out)
+        self._log(f'got info: {info}')
+
+        self._vulns = int(info['vulns'])
+        self._puts = int(info['puts'])
+        self._gets = int(info['gets'])
+        self._timeout = int(info['timeout'])
+        self._attack_data = bool(info['attack_data'])
+
+        assert self._puts > 0, f'checker {self._name}: invalid puts: {self._puts}'
+        assert self._gets > 0, f'checker {self._name}: invalid puts: {self._gets}'
+        assert 60 > self._timeout > 0, f'checker {self._name}: invalid timeout: {self._timeout}'
+
+    def _log(self, message: str):
+        with OUT_LOCK:
+            print(f'[{current_thread().name}] checker {self._name}: {message}')
+
+    def _run_command(self, command: List[str]) -> Tuple[str, str]:
+        try:
+            p = subprocess.run(command, capture_output=True, check=False, timeout=self._timeout)
+        except subprocess.TimeoutExpired:
+            self._log('command timeout expired')
+            raise
+
+        out = p.stdout.decode()
+        err = p.stderr.decode()
+
+        self._log(f'stdout:\n{out}\nstderr:\n{err}')
+
+        if p.returncode != 101:
+            self._log(f'bad return code: {p.returncode}')
+            exit(1)
+
+        return out, err
+
+    def check(self):
+        self._log('running CHECK')
+        cmd = [str(self._exe_path), 'check', HOST]
+        self._run_command(cmd)
+
+    def put(self, flag: str, flag_id: str, vuln: int):
+        self._log(f'running PUT, flag={flag} flag_id={flag_id} vuln={vuln}')
+        cmd = [str(self._exe_path), 'put', HOST, flag_id, flag, str(vuln)]
+        out, err = self._run_command(cmd)
+
+        assert out, f'checker {self._name}: stdout is empty'
+
+        new_flag_id = err
+        assert new_flag_id, f'checker {self._name}: returned flag_id is empty'
+
+        if self._attack_data:
+            assert flag not in out, f'checker {self._name}: Flag is leaked in public data'
+
+        return new_flag_id
+
+    def get(self, flag: str, flag_id: str, vuln: int):
+        self._log(f'running GET, flag={flag} flag_id={flag_id} vuln={vuln}')
+        cmd = [str(self._exe_path), 'get', HOST, flag_id, flag, str(vuln)]
+        self._run_command(cmd)
+
+    def run_all(self):
+        self._log('running all actions')
+        self.check()
+
+        for vuln in range(1, self._vulns + 1):
+            flag = generate_flag(self._name)
+            flag_id = self.put(flag=flag, flag_id=secrets.token_hex(16), vuln=vuln)
+            self.get(flag, flag_id, vuln)
+
+    def __str__(self):
+        return f'Checker {self._name}'
+
+
+class Service:
+    def __init__(self, name: str):
+        self._name = name
+        self._dc_path = SERVICES_PATH / self._name / 'docker-compose.yml'
+        assert self._dc_path.exists(), f'service {self._name}: docker-compose.yml missing'
+
+        self._checker = Checker(self._name)
+
+    def _log(self, message: str):
+        with OUT_LOCK:
+            print(f'[{current_thread().name}] service {self._name}: {message}')
+
+    def _run_dc(self, *args):
+        cmd = ['docker-compose', '-f', str(self._dc_path)] + list(args)
+        subprocess.run(cmd, check=True)
+
+    def up(self):
+        self._log('starting')
+        self._run_dc('up', '--build', '-d')
+
+    def logs(self):
+        self._log('printing logs')
+        self._run_dc('logs', '--tail', '2000')
+
+    def down(self):
+        self._log('stopping')
+        self._run_dc('down', '-v')
+
+    def _run_subtask(self, cnt):
+        for _ in range(cnt):
+            self._checker.run_all()
+
+    def validate_checker(self):
+        self._log('validating checker')
+
+        cnt_threads = max(1, min(8, RUNS // 16))
+        per_thread = RUNS // cnt_threads
+        tasks = RUNS
+        threads = []
+
+        while tasks:
+            cnt = min(tasks, per_thread)
+            t = Thread(target=self._run_subtask, args=(cnt,))
+            t.start()
+            threads.append(t)
+            tasks -= per_thread
+
+        for t in threads:
+            t.join()
+
+    def __str__(self):
+        return f'Service {self._name}'
+
+
+def get_services() -> List[Service]:
+    if os.getenv('SERVICE') == 'all':
+        result = list(
+            Service(service_path.name) for service_path in SERVICES_PATH.iterdir()
+            if service_path.name[0] != '.' and service_path.is_dir()
+        )
+    else:
+        result = [Service(os.environ['SERVICE'])]
+
+    with OUT_LOCK:
+        print('Got services:', ', '.join(map(str, result)))
+    return result
+
+
+def list_services(_args):
+    get_services()
+
+
+def start_services(_args):
+    for service in get_services():
+        service.up()
+
+
+def stop_services(_args):
+    for service in get_services():
+        service.down()
+
+
+def logs_services(_args):
+    for service in get_services():
+        service.logs()
+
+
+def validate_checkers(_args):
+    for service in get_services():
+        service.validate_checker()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Validate checkers for A&D. '
+                    'Host & number of runs are passed with HOST and RUNS env vars'
+    )
+    subparsers = parser.add_subparsers()
+
+    list_parser = subparsers.add_parser(
+        'list',
+        help='List services to test',
+    )
+    list_parser.set_defaults(func=list_services)
+
+    up_parser = subparsers.add_parser(
+        'up',
+        help='Start services',
+    )
+    up_parser.set_defaults(func=start_services)
+
+    down_parser = subparsers.add_parser(
+        'down',
+        help='Stop services',
+    )
+    down_parser.set_defaults(func=stop_services)
+
+    logs_parser = subparsers.add_parser(
+        'logs',
+        help='Print logs for services',
+    )
+    logs_parser.set_defaults(func=logs_services)
+
+    check_parser = subparsers.add_parser(
+        'check',
+        help='Run checkers validation',
+    )
+    check_parser.set_defaults(func=validate_checkers)
+
+    parsed = parser.parse_args()
+    try:
+        parsed.func(parsed)
+    except AssertionError:
+        raise
+    except Exception as e:
+        tb = traceback.format_exc()
+        print('Got exception:', e, tb)
+        exit(1)
