411abd87af04fe041fb4ed6cbc8203e5e69d7c7b
jnovikov <johnnovikov0@gmail.com>
2020-10-15 14:17:02 +0300

    Moim: fixed checker.py to check flag in pdf tickets

--- checkers/moim/checker.py
+++ checkers/moim/checker.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import tempfile
+
 from gevent import monkey
 
 monkey.patch_all()
@@ -6,9 +8,11 @@ monkey.patch_all()
 import sys
 import json
 import os
+import textract
 from random import randint, choice
 import checklib
 from faker import Faker
+
 from moim_lib import *
 
 anime_img_urls = [
@@ -33,6 +37,44 @@ class Checker(BaseChecker):
         images_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
         return [os.path.join(images_dir, x) for x in os.listdir(images_dir) if not x.startswith('.')]
 
+    def remap(self, l, key='id'):
+        d = {}
+        for v in l:
+            d[v[key]] = v
+        return d
+
+    def extract_text(self, file_resp):
+        try:
+            ntf = tempfile.NamedTemporaryFile(delete=False, suffix='.pdf')
+            ntf.write(file_resp.content)
+            ntf.close()
+            data = textract.process(ntf.name, encoding='utf-8')
+            os.unlink(ntf.name)
+        except Exception:
+            return b''
+        return data
+
+    def create_meeting_with_image(self, desc, sess, title, capacity):
+        params = {'height': f'{randint(20, 60)}%', 'width': f'{randint(20, 60)}%'}
+        image_file = None
+        image_url = None
+        if randint(0, 1) == 0:
+            image_file = open(choice(self.get_images()), 'rb')
+        else:
+            image_url = choice(anime_img_urls)
+        meeting_data = self.mch.create_meeting(sess, title=title, desc=desc, capacity=capacity, image_file=image_file,
+                                               image_url=image_url, image_params=params)
+        return meeting_data
+
+    def check_ticket_content(self, flag, ticket_data_resp):
+        self.assert_eq(ticket_data_resp.headers.get('Content-Type'), 'application/pdf', 'Failed to download ticket',
+                       status=Status.MUMBLE)
+        self.assert_gt(int(ticket_data_resp.headers.get('Content-Length', 0)), 0, 'Failed to download ticket',
+                       status=Status.MUMBLE)
+        self.assert_in(flag.encode(), self.extract_text(ticket_data_resp), 'Failed to read ticket data',
+                       status=Status.MUMBLE,
+                       )
+
     def check_response(self, r: requests.Response, public: str, status=checklib.status.Status.MUMBLE):
         try:
             error = r.json()['error']
@@ -48,12 +90,6 @@ class Checker(BaseChecker):
             super(Checker, self).action(action, *args, **kwargs)
         except requests.exceptions.ConnectionError:
             self.cquit(Status.DOWN, 'Connection error', 'Got requests connection error')
-
-    def remap(self, l, key='id'):
-        d = {}
-        for v in l:
-            d[v[key]] = v
-        return d
 
     def check(self):
         u, p, _ = self.mch.register_user()
@@ -87,10 +123,7 @@ class Checker(BaseChecker):
         self.assert_neq(ticket_url, None, 'Failed to add sync member', status=Status.MUMBLE)
 
         ticket_data_resp = self.mch.get_ticket(client_sess, ticket_url)
-        self.assert_eq(ticket_data_resp.headers.get('Content-Type'), 'application/pdf', 'Failed to download ticket',
-                       status=Status.MUMBLE)
-        self.assert_gt(int(ticket_data_resp.headers.get('Content-Length', 0)), 0, 'Failed to download ticket',
-                       status=Status.MUMBLE)
+        self.check_ticket_content(fake_flag, ticket_data_resp)
 
         members_data = self.mch.list_members(sess, meeting_id)
         member = self.remap(members_data, key='public_id').get(public_id)
@@ -119,18 +152,6 @@ class Checker(BaseChecker):
         }
         self.cquit(Status.OK, f'sync_{meeting_id}', json.dumps(full_data))
 
-    def create_meeting_with_image(self, desc, sess, title, capacity):
-        params = {'height': f'{randint(20, 60)}%', 'width': f'{randint(20, 60)}%'}
-        image_file = None
-        image_url = None
-        if randint(0, 1) == 0:
-            image_file = open(choice(self.get_images()), 'rb')
-        else:
-            image_url = choice(anime_img_urls)
-        meeting_data = self.mch.create_meeting(sess, title=title, desc=desc, capacity=capacity, image_file=image_file,
-                                               image_url=image_url, image_params=params)
-        return meeting_data
-
     def get(self, flag_id, flag, vuln):
         data = json.loads(flag_id)
         u, p = data['u'], data['p']
@@ -146,10 +167,7 @@ class Checker(BaseChecker):
         self.assert_eq(ticket_data.get('nickname'), flag, 'Failed to get ticket data', status=Status.CORRUPT)
         ticket_url = ticket_data.get('ticket_url')
         ticket_data_resp = self.mch.get_ticket(client_sess, ticket_url)
-        self.assert_eq(ticket_data_resp.headers.get('Content-Type'), 'application/pdf', 'Failed to download ticket',
-                       status=Status.MUMBLE)
-        self.assert_gt(int(ticket_data_resp.headers.get('Content-Length', 0)), 0, 'Failed to download ticket',
-                       status=Status.MUMBLE)
+        self.check_ticket_content(flag, ticket_data_resp)
         self.cquit(Status.OK)
 
 
