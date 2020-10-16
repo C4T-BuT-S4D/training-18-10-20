ad049b47626719ddcd03ecdab3aa97b1492e647d
jnovikov <johnnovikov0@gmail.com>
2020-10-16 15:36:15 +0300

    Moim: add more workers

--- services/moim/src/entrypoint.sh
+++ services/moim/src/entrypoint.sh
@@ -5,5 +5,5 @@
 cd /var/www/html &&\
 php artisan migrate --force &&\
 chown -R www-data /storage &&\
-php artisan queue:work --sleep=1 --max-jobs=30 &
+php artisan queue:work --sleep=1 --max-jobs=40 &
 apache2-foreground
